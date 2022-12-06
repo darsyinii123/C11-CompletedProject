3dcaabf9199ed6eec4b3532a5d099a59dc461966
darsyinii123 <118985084+darsyinii123@users.noreply.github.com>
2022-12-06 12:07:30 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,40 +1,51 @@
-var box;
+var path,boy, leftBoundary,rightBoundary;
+var pathImg,boyImg;
+var i;
 
-function setup() {
-  createCanvas(400,400);
-  box = createSprite(200,200,30,30);
-
+function preload(){
+  pathImg = loadImage("path.png");
+  boyImg = loadAnimation("Runner-1.png","Runner-2.png");
 }
 
-function draw() 
-{
-   
+function setup(){
+  
+  createCanvas(400,400);
+  
+// Moving background
+  path=createSprite(200,200);
+  path.addImage(pathImg);
+  path.velocityY = 4;
+  path.scale=1.2;
 
-  if (keyIsDown(RIGHT_ARROW)) 
-  {
-    background("red");
-  }
+//creating boy running
+    boy = createSprite(180,340,30,30);
+    boy.scale=0.08;
+    boy.addAnimation("JakeRunning",boyImg);
+      
 
-    if (keyIsDown(LEFT_ARROW)) 
-  {
-    background("yellow");
-    
-  }
- 
-    if (keyIsDown(UP_ARROW)) 
-  {
-    background("blue");
-   
-  }
+  leftBoundary=createSprite(0,0,100,800);
+  leftBoundary.visible = false;
 
-  if (keyIsDown(DOWN_ARROW)) 
-  {
-    background("green");
-  }
 
+  rightBoundary=createSprite(410,0,100,800);
+  rightBoundary.visible = false;
+}
+
+function draw() {
+  background(0);
+  path.velocityY = 4;
+  
+  boy.x = World.mouseX;
+  
+  edges= createEdgeSprites();
+  boy.collide(edges[3]);
+  boy.collide(leftBoundary);
+  boy.collide(rightBoundary);
+
+  if(path.y > 400 ){
+path.y = height/2;}
+
+
+  
   drawSprites();
 }
-
-
-
-
